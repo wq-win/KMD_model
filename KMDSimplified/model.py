451dd068a40dae96e24bb7bf3b1a2a71a678a947
wangq635 <643797037@qq.com>
2024-01-22 22:25:36 +0800

    add dan potentiated kc

--- KMDSimplified/model.py
+++ KMDSimplified/model.py
@@ -1,21 +1,24 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+def sigmoid(x):  
+    return 1 / (1 + np.exp(-np.array(x)))
 
 class KMDmodel():
     def __init__(self,n_K=6,n_M=3,n_D=3) -> None:
-        self.task_input_KC = {'0':[0,1],'2':[1,5],'1':[2,4]}
-        self.task_output_KC = {'0':[1,2,4],'2':[0,4,1],'1':[1,4,5]}
-        self.potential_KC = {'0':[3,4],'1':[1,3],'2':[1,3]}
+        self.task_input_KC = {'0':[0,1],'1':[2,3],'2':[4,5]}
+        self.task_output_KC = {'0':[2,4],'1':[1,5],'2':[1,3]}
+        self.potential_KC = {'0':[0,1],'1':[2,3],'2':[4,5]}
         self.KC = np.zeros(n_K,dtype=float)
         self.MBON = np.zeros(n_M,dtype=float)
         self.DAN = np.zeros(n_D,dtype=float)
-        self.threshold = 1
+        self.threshold = sigmoid(1)
         self.t = 0
 
     def activatedKC(self):
     # 返回到达阈值KC下标
         record = []
+        self.KC = sigmoid(self.KC)
         for index,value in enumerate(self.KC):
             if value >= self.threshold:
                 record.append(index)        
@@ -23,13 +26,13 @@ class KMDmodel():
     
     def activatedMBON(self,inputKC,outputMBON):
         self.MBON = np.zeros(len(self.MBON))
-        self.DAN = np.zeros(len(self.DAN))
+        # self.DAN = np.zeros(len(self.DAN))
     # 将激活KC对应的MBON输出
         if set(inputKC).issubset(set(self.activatedKC())):
             self.MBON[outputMBON] = 1
-            self.DAN[outputMBON] = 1
+            # self.DAN[outputMBON] = 1
 
-    def activatedDAN(self,inputMBON):
+    def activatedDAN(self,inputMBON,inputDAN):
         tokc = []
         pokc = []
         self.KC = np.zeros(len(self.KC))
@@ -37,11 +40,15 @@ class KMDmodel():
         for i, value in enumerate(inputMBON):
             if value == 1:
                 tokc = self.task_output_KC[str(i)]
-                pokc = self.potential_KC[str(i)]
+                # pokc = self.potential_KC[str(i)]
+        for i, value in enumerate(inputDAN):
+            if value == 1:
+                pokc = self.potential_KC[str(i)]        
         for i in tokc:
-            self.KC[i] = 1
+            self.KC[i] += 1
         for i in pokc:
-            self.KC[i] += 0.4
+            # self.KC[i] += 0.4
+            self.KC[i] += 1
         self.KC = np.array(self.KC)
         self.KC[self.KC>1] = 1
          
@@ -49,36 +56,41 @@ class KMDmodel():
     def step(self):
         n = self.t
         self.activatedMBON(self.task_input_KC[str(n)],n)
-        self.activatedDAN(self.MBON)
+        self.activatedDAN(self.MBON,self.DAN)
         # print(self.KC,self.MBON,self.DAN)
         self.t += 1
         self.t %= 3   
         # print(n)     
 
 kmd = KMDmodel()
-kmd.KC = [1,1,0,0,1,1]
-
+kmd.KC = [1,1,0,0,0,0]
+kmd.DAN = [0,1,0]
 kc,mbon,dan = [],[],[]
-for i in range(100):
+for i in range(20):
     kc.append(kmd.KC)
     mbon.append(kmd.MBON)
     kmd.step()
 
-# plt.figure(1)
+plt.figure(1)
 # plt.plot(range(100), mbon)
-
-# mbon = np.array(mbon)
-# plt.subplot(311)
-# plt.plot(range(100), mbon.T[0])
-# plt.subplot(312)
-# plt.plot(range(100), mbon.T[1])
-# plt.subplot(313)
-# plt.plot(range(100), mbon.T[2])
+# plt.xlabel('all mbon')
+# plt.show()
+b=range(20)
+mbon = np.array(mbon)
+plt.subplot(311)
+plt.plot(b, mbon.T[0])
+plt.subplot(312)
+plt.plot(b, mbon.T[1])
+plt.subplot(313)
+plt.plot(b, mbon.T[2])
+plt.xlabel('mbon')
+# plt.show()
 
 kc = np.array(kc)
 plt.figure(2)
 for i in range(6):
     a = 611+i
     plt.subplot(a)
-    plt.plot(range(100),kc.T[i])
+    plt.plot(b,kc.T[i])
+plt.xlabel('kc')
 plt.show()       
