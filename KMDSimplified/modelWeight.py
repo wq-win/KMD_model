f4b245b97ff9326acc35baf2aae38b0cbedc47dc
wangq635 <643797037@qq.com>
2024-01-26 21:03:38 +0800

    add derivative

--- KMDSimplified/modelWeight.py
+++ KMDSimplified/modelWeight.py
@@ -12,6 +12,24 @@ def rk4(h, y, inputs, f):
              y[1] + h / 6 * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1])]
     
     return y_new
+def rk4(h, y, inputs, f):
+    '''
+    用于数值积分的rk4函数。
+    args:
+        h - 步长
+        y - 当前状态量
+        inputs - 外界对系统的输入
+        f - 常微分或偏微分方程
+    return:
+        y_new - 新的状态量,即经过h时间之后的状态量
+    '''
+    k1 = f(y, inputs)
+    k2 = f(y + h / 2 * k1, inputs)
+    k3 = f(y + h / 2 * k2, inputs)
+    k4 = f(y + h * k3, inputs)
+
+    y_new = y + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+    return y_new
 
 
 class Synapse:
@@ -25,24 +43,22 @@ class Synapse:
         # 超参数
         self.alpha = 0.5
         self.tau = 0.5
+        self.dt = 1
 
+    def derivative(self, state, inputs=0):
+        w = state
+        Dw = np.zeros_like(self.weight)
+        for r in range(self.weight.shape[0]):
+            for c in range(self.weight.shape[1]):
+                Dw[r][c] = (self.preneuron[r] * self.postneuron[c] - w[r][c]) * self.isreward
+        return Dw        
+            
         
-    def step(self):
-        s = self.weight.shape
-        for r in range(s[0]):
-            for c in range(s[1]):
-                self.weight[r][c] = (self.preneuron[r] * self.postneuron[c] - self.weight[r][c]) * self.isreward
-
+    def step(self, state, dt, inputs=0):
+        statenew = rk4(dt, state, inputs, self.derivative)
+        return statenew
         
 KCtoMBON = Synapse(6,3)
-simlen = range(100)
-weightlist = []
-for i in simlen:
-    KCtoMBON.preneuron = np.array([random.choice([0,1]) for _ in range(6)])
-    KCtoMBON.postneuron = np.array([random.choice([0,1]) for _ in range(3)])
-    KCtoMBON.isreward = np.array(random.choice([0,1]))
-    weightlist.append(KCtoMBON.weight[0][0])
-    KCtoMBON.step()
-# print(KCtoMBON.weight)
-plt.plot(simlen,weightlist)
-plt.show()
+w = KCtoMBON.weight
+KCtoMBON.step(w,1)
+
